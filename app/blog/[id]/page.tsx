22fd103e1a74f0d5d4310aad20ff3f492c062502
kruvv <kruvv@mail.ru>
2025-09-04 17:51:11 +0300

    Add view post

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,17 @@
 import { Metadata } from "next";
 
+async function getData(id: string) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    },
+  );
+  return response.json();
+}
+
 type Props = {
   params: {
     id: string;
@@ -13,6 +25,12 @@ export async function generateMetadata({
     title: id,
   };
 }
-export default function Post({ params: { id } }: Props) {
-  return <h1>Post page {id}</h1>;
+export default async function Post({ params: { id } }: Props) {
+  const post = await getData(id);
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
+  );
 }
